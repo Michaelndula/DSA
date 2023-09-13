3a6a60e92f90a125b77c54ee047098111172f140
Michael Ndula <michaelndula@gmail.com>
2023-09-14 00:39:14 +0300

    0(log2(n)) time complexity

--- time_complexity/src/main/java/time_complexity/Element.java
+++ time_complexity/src/main/java/time_complexity/Element.java
@@ -1,10 +1,10 @@
 package time_complexity;
 
-//public class Element {
-//    public static void main(String[] args){
-//        System.out.println("Hello World!!");
-//    }
-//}
+public class Element {
+   public static void main(String[] args){
+       System.out.println("Hello World!!");
+   }
+}
 
 // Time Complexity: In the above code “Hello World” is printed only once on the screen.
 // So, the time complexity is constant: O(1) i.e. every time a constant amount of time is required to execute code, no matter which operating system or which machine configurations you are using.
@@ -22,4 +22,17 @@ class GFG{
 
 // Time Complexity: In the above code “Hello World !!!” is printed only n times on the screen, as the value of n can change.
 // So, the time complexity is linear: O(n) i.e. every time, a linear amount of time is required to execute code.
+// Auxiliary Space: O(1)
+
+class GFG1{
+    public static void main(String[] args){
+        int i, n = 8;
+
+        for(i = 1; i <= n; i=i*2){
+            System.out.println("Hello World");
+        }
+    }
+}
+
+// Time Complexity: O(log2(n))
 // Auxiliary Space: O(1)
